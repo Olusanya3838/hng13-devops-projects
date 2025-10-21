80251ee25696015dbe85680b9180a9ab46bded38
Olajide Olusanya <OOlusanya@providusbank.com>
2025-10-21 09:32:57 +0100

    Initial Node.js app with Dockerfile

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from your Dockerized Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
