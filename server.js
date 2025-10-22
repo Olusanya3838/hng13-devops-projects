80011b3ff846b138c74fba0080657ea8bb33ee82
Oluasanya Olajide <111055604+Olusanya3838@users.noreply.github.com>
2025-10-22 16:56:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('🚀 Hello from your Dockerized Node.js App!');
@@ -9,3 +9,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
